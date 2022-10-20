2a8b97659680cd2a1f02be7a746991abad001f7b
Scott Sauber <stsauber@gmail.com>
2022-10-20 09:31:41 -0500

    add cdnug background tasks talk

---
+++ 2022-10-20-CDNUG-TheBackgroundOnBackgroundTasksInDotNet6/src/Shared/CacheService.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace Shared;
+
+public interface ICacheService
+{
+    Task RefreshDashboardCacheAsync();
+    void RemoveDashboardCache();
+}
+
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _cache;
+    private readonly ILogger<CacheService> _logger;
+
+    public CacheService(IDistributedCache cache, ILogger<CacheService> logger)
+    {
+        _cache = cache;
+        _logger = logger;
+    }
+        
+    public async Task RefreshDashboardCacheAsync()
+    {
+        var rng = Random.Shared;
+        var dashboardResult = new DashboardResult
+        {
+            AverageSale = rng.Next(1, 2_000),
+            LastUpdated = DateTime.UtcNow,
+            NumberOfSales = rng.Next(1, 10_000)
+        };
+        var encodedDashboard = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(dashboardResult));
+
+        await _cache.SetAsync(CacheKeys.Dashboard, encodedDashboard, new DistributedCacheEntryOptions());
+
+        _logger.LogInformation("{cacheKey} cache refreshed", CacheKeys.Dashboard);
+    }
+
+    public void RemoveDashboardCache()
+    {
+        _cache.Remove(CacheKeys.Dashboard);
+    }
+}
