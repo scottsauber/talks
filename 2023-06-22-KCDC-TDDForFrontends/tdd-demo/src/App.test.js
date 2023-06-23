92d5d32a76b9c9c50e5c4b89be9aea40b286b188
Scott Sauber <stsauber@gmail.com>
2023-06-23 09:41:06 -0500

    updates

--- 2023-06-22-KCDC-TDDForFrontends/tdd-demo/src/App.test.js
+++ 2023-06-22-KCDC-TDDForFrontends/tdd-demo/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-    render(<App />);
+    render(<App/>);
     const linkElement = screen.getByText("Learn React");
     expect(linkElement).toBeInTheDocument();
 });
