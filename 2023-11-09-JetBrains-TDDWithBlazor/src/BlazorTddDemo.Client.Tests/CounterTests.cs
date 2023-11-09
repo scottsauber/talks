e04398a8fbde3578ef714ec4f2eadc261970364a
scottsauber <stsauber@gmail.com>
2023-11-08 21:22:14 -0600

    latest updates

---
+++ 2023-11-09-JetBrains-TDDWithBlazor/src/BlazorTddDemo.Client.Tests/CounterTests.cs
@@ -0,0 +1,35 @@
+using Bunit;
+using Counter = BlazorTddDemo.Client.Pages.Counter;
+using FluentAssertions;
+
+namespace BlazorTddDemo.Client.Tests;
+
+[UsesVerify]
+public class CounterTests
+{
+    [Fact]
+    public void ShouldIncrementCountWhenClickingButton()
+    {
+        using var testContext = new TestContext();
+        
+        var component = testContext.RenderComponent<Counter>();
+        var button = component.Find("button");
+        button.Click();
+
+        var currentCount = component.Find("[role='status']");
+        currentCount.TextContent.Should().Be("Current count: 1");
+    }
+    
+    [Fact]
+    public Task SnapshotExample()
+    {
+        VerifyBunit.Initialize();
+        using var testContext = new TestContext();
+        
+        var component = testContext.RenderComponent<Counter>();
+        var button = component.Find("button");
+        button.Click();
+
+        return Verify(component);
+    }
+}
