f3557078fa3a6dd8b1b52ee02024f6281a783fc0
Scott Sauber <stsauber@gmail.com>
2024-09-11 00:13:32 -0500

    add logging

--- 2024-09-11-IMAUG-Dotnet-Aspire/src/DotNetAspireDemo/DotNetAspireDemo.AppHost/Program.cs
+++ 2024-09-11-IMAUG-Dotnet-Aspire/src/DotNetAspireDemo/DotNetAspireDemo.AppHost/Program.cs
@@ -1,6 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// var sql = builder.AddSqlServer("sql");
+// var sqldb = sql.AddDatabase("sqldb");
+
 var apiService = builder.AddProject<Projects.DotNetAspireDemo_ApiService>("apiservice");
+    // .WithReference(sqldb);
 
 builder.AddProject<Projects.DotNetAspireDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
