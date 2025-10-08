840330dc893a1c4a9a057a6d3e577927f5b11835
Scott Sauber <stsauber@gmail.com>
2025-10-08 10:20:49 -0400

    latest changes

---
+++ 2025-10-7-DevIntersectionOrlando-BackgroundOnBackgroundTasks/src/DHangfire/Controllers/HangfireController.cs
@@ -0,0 +1,25 @@
+// ReSharper disable All
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DHangfire.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HangfireController(IBackgroundJobClient backgroundJobClient)
+{
+    [HttpGet]
+    public void Trigger()
+    {
+        backgroundJobClient.Enqueue(
+            () => TriggeredFromControllerAsync("Hello from the Controller!"));
+    }
+
+    [JobDisplayName("Triggered from Controller with - {0}")]
+    public static async Task TriggeredFromControllerAsync(string value)
+    {
+        throw new Exception();
+        await Task.Delay(5000);
+        Console.WriteLine(value);
+    }
+}
---
+++ 2025-10-7-DevIntersectionOrlando-BackgroundOnBackgroundTasks/src/DHangfire/Program.cs
@@ -0,0 +1,49 @@
+using DHangfire;
+using Hangfire;
+using Hangfire.SqlServer;
+
+var builder = WebApplication.CreateBuilder();
+
+var hangfireConnectionString = builder.Configuration.GetConnectionString("Hangfire")!;
+await DatabaseHelpers.CreateHangfireDatabaseIfItDoesntExistAsync(hangfireConnectionString);
+builder.Services.AddControllers();
+builder.Services.AddDemoServices();
+builder.Services.AddSingleton<IJobService, JobService>();
+builder.Services.AddHangfire(hangfireConfig =>
+{
+    hangfireConfig
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(hangfireConnectionString,
+            new SqlServerStorageOptions
+            {
+                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                QueuePollInterval = TimeSpan.Zero,
+                UseRecommendedIsolationLevel = true,
+                DisableGlobalLocks = true
+            });
+});
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.MapHangfireDashboard(new DashboardOptions
+{
+    Authorization = 
+});
+
+RecurringJob.AddOrUpdate("Inline Method",
+    () => Console.WriteLine("Inline method recurring every minute!"),
+    Cron.Minutely);
+
+RecurringJob.AddOrUpdate("Custom Job Name",
+    () => app.Services.GetRequiredService<IJobService>().ExternalJob(),
+    Cron.Minutely
+);
+
+app.Run();
